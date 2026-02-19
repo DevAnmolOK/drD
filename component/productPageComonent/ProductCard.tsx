d7fb16459581fef29ef5be3be4d98b8fa27af9ad
Anmol <anmol.webhopers@gmail.com>
2026-02-19 16:01:39 +0530

    merg to main repo

--- component/productPageComonent/ProductCard.tsx
+++ component/productPageComonent/ProductCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-// import { calculatePtsPtr } from "@/utils/pharmaPricing";
+// import { calculatePtsPtr } from "@/utils/pharmaPricing";/
 import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
