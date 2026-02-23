e8647eba5a280537e9ea9d8f37cc13cca2134501
whashishkumar <wh.ashishkumar@gmail.com>
2026-02-23 10:26:06 +0530

    fix header

--- component/homePageComponent/HeroSection.tsx
+++ component/homePageComponent/HeroSection.tsx
@@ -6,32 +6,45 @@ export default async function HeroSection({ homeBannerSection }: any) {
   return (
     <>
       <div className="h-[51rem]  relative w-full flex  justify-center  items-center   border-red-500 -mt-[6.35rem] ">
-        <div
+        {/* <div
           className=" sm:bg-[position:100%_100%] bg-[position:100%_100%] absolute inset-0  bg-no-repeat overflow-hidden sm:bg-[length:101%] max-[640px]:bg-[length:100%] "
           style={{
             backgroundImage: `url(${background?.imageSrc})`,
           }}
-        >
-          {/* <div className="absolute inset-0 bg-gradient-to-r from-[#006511] to-transparent opacity-100"></div> */}
+        > */}
+        <div
+  className="
+  absolute inset-0
+    bg-no-repeat
+    bg-cover
+    bg-[position:50%_20%]     
+    sm:bg-center              
+    md:bg-[position:100%_100%]
+  "
+  style={{
+    backgroundImage: `url(${background?.imageSrc})`,
+  }}
+>
+          <div className="absolute inset-0 bg-gradient-to-r from-[#006511] to-transparent opacity-10"></div>
         </div>
-        <div className=" relative z-10  w-full flex text-white  md:mb-0 sm:pb-[3rem] max-w-[101.625rem]  mt-[15rem]">
-          <div className="   h-full  items-center justify-center   flex flex-col   lg:mt-[1.25rem]">
+        <div className="px-8  relative z-10  w-full flex text-white  md:mb-0 sm:pb-[3rem] max-w-[101.625rem]  md:mt-[15rem]">
+          <div className=" h-full  items-center justify-center   flex flex-col   lg:mt-[1.25rem]">
             <div className=" flex flex-col pb-[2rem]">
-              <h1 className=" text-[5.25rem] align-middle  sm:leading-[1.1190] font-bold     w-[60%] text-white mt-[1rem] sm:mt-0  ">
+              <h1 className=" text-[2rem] md:text-[5.25rem] align-middle  sm:leading-[1.1190] font-bold w-full lg:w-[60%] text-white mt-[1rem] sm:mt-0  ">
                 {title?.normal}
               </h1>
-              <div className="flex sm:flex-row flex-col sm:mt-20 mt-8 w-[40%]  gap-10 ">
+              <div className="flex sm:flex-row flex-col sm:mt-20 mt-8 w-full lg:w-[40%]  gap-10 ">
                 <Link href={buttonLink} className="bg-white cursor-pointer w-[7.125rem] h-fit sm:px-4 py-1 align-middle text-black  flex gap-1 leading-[1.6250] text-base font-normal items-center text-nowrap justify-center cursor-pointer ">  
                   {badgeText}
                 </Link>
-
-                <div className=" text-base  leading-[1.6250] font-normal align-middle ">
+                <div className="text-base  leading-[1.6250] font-normal align-middle ">
                  {description}
                 </div>
               </div>
             </div>
           </div>
         </div>
+        
       </div>
     </>
   );
--- component/homePageComponent/LeadingMedicalSection.tsx
+++ component/homePageComponent/LeadingMedicalSection.tsx
@@ -3,24 +3,21 @@
 import { FiAward, FiCheckCircle, FiPackage, FiUsers } from "react-icons/fi";
 import Image from "next/image";
 export default function TrustedSection({ homeTrustedBy }: any) {
-
   const { heading_start, heading_bold, heading_end, stats } = homeTrustedBy.data || {};
 
   return (
     <section className="w-full bg-gradient-to-r from-[#253746]  to-[#162836] text-white py-20 px-6">
       <div className="wrapper mx-auto ">
-        {/* Heading */}
         <h2 className=" text-[3rem] font-normal  leading-[1.1667] align-middle capitalize tracking-[1.5px]  mb-16">
           {heading_start}
           <span className="font-bold mx-2">{heading_bold}</span> {heading_end}
         </h2>
-
         {/* Stats */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  r divide-y sm:divide-y-0 lg:divide-x divide-white/20">
-          {stats.map((item:any, index:any) => (
+          {stats?.map((item:any, index:any) => (
             <div
               key={index}
-              className={`flex flex-col ${index === 0 ? "items-start justify-start" : " items-center justify-center"}  py-10 lg:py-0`}
+              className={`flex flex-col ${index === 0 ? "lg:items-start items-center justify-center" : " items-center justify-center"}  py-10 lg:py-0`}
             >
               <div className="">
                 <div className="mb-2 h-[3rem] w-[3rem] relative overflow-hidden">
@@ -32,12 +29,10 @@ export default function TrustedSection({ homeTrustedBy }: any) {
                     className=" object-contain"
                   />
                 </div>
-
                 <div className="text-[3rem] font-normal  mb-2 align-middle ">
                   {item.number}
                 </div>
-
-                <p className="text-base  font-normal align-middle leading-[180%]">
+                <p className="text-base  font-normal align-middle leading-[180%] max-w-[10rem] md:w-full">
                   {item.label}
                 </p>
               </div>
