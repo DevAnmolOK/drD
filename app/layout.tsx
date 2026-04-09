a53a53beb29d8e68ff9df9acbb0c886222538c2f
Anmol <anmol.webhopers@gmail.com>
2026-04-09 17:28:33 +0530

    implement google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import NavigationBar from "../component/layOut/Header";
 import Footer from "@/component/layOut/Footer";
@@ -61,8 +62,50 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="7RfPynaXIu4_xDVY-mWN4NBiwPEsW1DRaa0vCH6tBos"
+        />
+        {/* Google tag (gtag.js)  */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=id=G-QCVHYXSSHL`}
+        />
+        {/* GA init */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QCVHYXSSHL');
+            `,
+          }}
+        />
+
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2NX8C6F');
+          `}
+        </Script>
       </head>
       <body className={`${sans.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-N2NX8C6F`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NavigationBar
           navBar={navBar}
           productMenu={productMenu}
